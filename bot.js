22af753788617fbb42b4bad0be83acb4be26f90d
MrHXER <42505508+MrHXER@users.noreply.github.com>
2018-08-19 06:42:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,19 +26,5 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('╚[════════════]╝')
   console.log('')
   console.log('')
-});
-//! KinG66S.❤#0045
-var KinG66S = {};//! KinG66S.❤#0045
-client.on('guildMemberRemove', member => {//! KinG66S.❤#0045
-KinG66S[member.id] = {roles: member.roles.array()};//! KinG66S.❤#0045
-});
-//! KinG66S.❤#0045 //! KinG66S.❤#0045 //! KinG66S.❤#0045 
-client.on('guildMemberAdd', member => {//! KinG66S.❤#0045
-if(!KinG66S[member.user.id]) return;//! KinG66S.❤#0045
-console.log(KinG66S[member.user.id].roles.length);//! KinG66S.❤#0045
-for(let i = 0; i < KinG66S[member.user.id].roles.length + 1; i++) {//! KinG66S.❤#0045
-member.addRole(KinG66S[member.user.id].roles.shift());//! KinG66S.❤#0045
-}//! KinG66S.❤#0045
-});//! KinG66S.❤#0045
 
 client.login(process.env.BOT_TOKEN);
